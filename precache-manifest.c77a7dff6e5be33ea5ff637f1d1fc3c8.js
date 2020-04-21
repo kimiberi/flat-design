ab7b8e40e2ce32c7e3230e9d5fb089ea4815ccf3
Kimberly Villarmente <you@example.com>
2020-04-21 20:49:05 +0800

    Updates

---
+++ precache-manifest.c77a7dff6e5be33ea5ff637f1d1fc3c8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c0ad52cf5fc9a85966cadaa336084b",
+    "url": "/flat-design/index.html"
+  },
+  {
+    "revision": "5c8cf47e46aa3150707a",
+    "url": "/flat-design/static/css/main.b3e88391.chunk.css"
+  },
+  {
+    "revision": "87a1fc6d8396416c00ea",
+    "url": "/flat-design/static/js/2.8a1ce77b.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/flat-design/static/js/2.8a1ce77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8cf47e46aa3150707a",
+    "url": "/flat-design/static/js/main.ff32b190.chunk.js"
+  },
+  {
+    "revision": "2faa9cdf7980eef49b23",
+    "url": "/flat-design/static/js/runtime-main.f8e88817.js"
+  },
+  {
+    "revision": "af1c504e8c6a83c9e828d327545a989a",
+    "url": "/flat-design/static/media/business-design.af1c504e.png"
+  },
+  {
+    "revision": "2fa1e0e98b5c063c606495f8573b7068",
+    "url": "/flat-design/static/media/co-employee.2fa1e0e9.png"
+  },
+  {
+    "revision": "44a5984f26423cd788cde30b0c3b030d",
+    "url": "/flat-design/static/media/darkpink-cloud.44a5984f.png"
+  },
+  {
+    "revision": "c01429696a3ac1dc61ce658ba642d707",
+    "url": "/flat-design/static/media/keyvisual.c0142969.png"
+  },
+  {
+    "revision": "5bb4ab66560a8b28d24aae958c12149e",
+    "url": "/flat-design/static/media/redforest.5bb4ab66.png"
+  },
+  {
+    "revision": "c804405cfeedaaf110f68fc8c46defe5",
+    "url": "/flat-design/static/media/white-cloud.c804405c.png"
+  },
+  {
+    "revision": "0f1bec21dff02a89bf87f15e353e159c",
+    "url": "/flat-design/static/media/woman-employee.0f1bec21.png"
+  },
+  {
+    "revision": "65cadf1f6aeea84cb685c3ed61113920",
+    "url": "/flat-design/static/media/young-employee.65cadf1f.png"
+  },
+  {
+    "revision": "53ebf430d32072ba9c161fb07ab6d74d",
+    "url": "/flat-design/static/media/young-man.53ebf430.png"
+  }
+]);
